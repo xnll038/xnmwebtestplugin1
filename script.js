a3338a3e714a20738b31c77d391c011f9c021817
xnll <144842245+xnll038@users.noreply.github.com>
2026-01-31 00:46:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 export default {
 	init(sandbox, api) {
-		console.log("PROVOD");
+		console.log("!PROVOD!");
+		console.log(sandbox);
+		console.log(api);
 	},
 	destroy(sandbox, api) {
 		console.log("unloaded");
